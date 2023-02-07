1f4c763cb79e5d2c8840968b23815b7d864fb7e8
Chuck Choi <hyunchoi94@gmail.com>
2023-02-06 19:41:12 -0800

    initial commit

---
+++ udemy-algorithm-course/5.28-same.js
@@ -0,0 +1,30 @@
+// write a function caled same, which accepts two arrays.
+// the function should return true if every value in the 
+// array has its corresponding value squared in the second array.
+// The frequency of values must be the same.
+
+// EXAMPLE:
+// same([1,2,3], [4,1,9]) -- true
+// same([1,2,3], [1,9]) -- false
+// same([1,2,1], [4,4,1]) -- false (must be same frequency)
+
+const same = (arr1, arr2) => {
+    if(arr1.length !== arr2.length) return false
+
+    const frequencyCounter1 = {}
+    const frequencyCounter2 = {}
+
+    for(const val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for(const val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+
+    for(let key in frequencyCounter1){
+        if(!frequencyCounter2[key ** 2]) return false
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) return false
+    }
+
+    return true
+}
